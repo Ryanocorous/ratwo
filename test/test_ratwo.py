763b8d7ed08096bf1584b94e617e196d633894df
Ryanocorous <Ryan.middlet@gmail.com>
2026-08-30 14:54:44 +0100

    Add initial test structure for test_ratwo.py

---
+++ test/test_ratwo.py
@@ -0,0 +1,9 @@
+import asyncio,sys,os
+sys.path.insert(0,os.path.dirname(os.path.dirname(__file__)))
+from ratwo import Ratwo
+
+async def search():
+async def memory():
+async def main():
+
+asyncio.run(main())
